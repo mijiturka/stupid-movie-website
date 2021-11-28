1457f12b81bf2db82f2ca42646542ef638d48d81
Rumyana <mijiturka@tuta.io>
2021-11-28 18:36:59 +0000

    Update film list template to the actual html required

--- generate-film-list/generate.py
+++ generate-film-list/generate.py
@@ -4,7 +4,7 @@ from pathlib import Path
 films = Path('./list.md').read_text().splitlines()
 
 env = jinja2.Environment(loader = jinja2.FileSystemLoader('templates'))
-template = env.get_template('film-list.html')
+template = env.get_template('films.html')
 
 html = template.render(films=films)
 print(html)
