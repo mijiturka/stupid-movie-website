8c5cfcaaabb260cba5a38e4ea7fbb06689507d4e
Rumyana <mijiturka@tuta.io>
2021-11-28 19:23:13 +0000

    Use a JSON file for the movie list
    We need to add the proper human-readable titles in addition to the usable names;
    we can keep the pairs in a JSON file.

--- generate-film-list/generate.py
+++ generate-film-list/generate.py
@@ -1,11 +1,12 @@
 import jinja2
+import json
 from pathlib import Path
 
-films = Path('./list.md').read_text().splitlines()
+films = json.loads(Path('./list.json').read_text())
 
 env = jinja2.Environment(loader = jinja2.FileSystemLoader('templates'))
 template = env.get_template('films.html')
 
-html = template.render(films=films)
+html = template.render(films=films['films'].keys())
 Path('./generated').mkdir(exist_ok=True)
 Path('./generated/films.html').write_text(html)
