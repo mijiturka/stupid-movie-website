2bec3ba4f11fe8363ad075e3933437a212f19257
Rumyana <mijiturka@tuta.io>
2021-11-30 22:29:35 +0000

    Refactor generate.py

--- generate-film-list/generate.py
+++ generate-film-list/generate.py
@@ -2,11 +2,17 @@ import jinja2
 import json
 from pathlib import Path
 
+
+def template():
+    env = jinja2.Environment(loader = jinja2.FileSystemLoader('templates'))
+    return env.get_template('films.html')
+
+def write(html, file_name):
+    Path('./generated').mkdir(exist_ok=True)
+    Path(f'./generated/{file_name}').write_text(html)
+
+# Get info on all films
 films = json.loads(Path('./list.json').read_text())
-
-env = jinja2.Environment(loader = jinja2.FileSystemLoader('templates'))
-template = env.get_template('films.html')
-
-html = template.render(films=films['films'].items())
-Path('./generated').mkdir(exist_ok=True)
-Path('./generated/films.html').write_text(html)
+# Generate a page with the full list of films
+html = template().render(films=films['films'].items())
+write(html, 'films.html')
