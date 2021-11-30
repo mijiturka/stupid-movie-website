1689208b11b6aafd94637b24f3ef99bb45d48eb6
Rumyana <mijiturka@tuta.io>
2021-11-30 22:50:40 +0000

    Generate a page with 20 randomly selected films

--- generate-film-list/generate.py
+++ generate-film-list/generate.py
@@ -1,7 +1,7 @@
 import jinja2
 import json
 from pathlib import Path
-
+import random
 
 def template():
     env = jinja2.Environment(loader = jinja2.FileSystemLoader('templates'))
@@ -12,7 +12,13 @@ def write(html, file_name):
     Path(f'./generated/{file_name}').write_text(html)
 
 # Get info on all films
-films = json.loads(Path('./list.json').read_text())
+all_films = json.loads(Path('./list.json').read_text())['films']
+
 # Generate a page with the full list of films
-html = template().render(films=films['films'].items())
+html = template().render(films=all_films.items())
 write(html, 'films.html')
+
+# Generate a page with 20 randomly selected films
+all_films_tuples = list(all_films.items())
+html = template().render(films=random.choices(all_films_tuples, k=20))
+write(html, 'films-1-20.html')
