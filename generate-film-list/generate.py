15b479d54ed62c772ad311441788d544bd1e312c
Rumyana <mijiturka@tuta.io>
2021-12-01 21:53:47 +0000

    Add "Prev" link
    Current solution is clunky; some more reading into how Jinja works
    would probably give a reasonable way to refactor generate.py.
    
    Inactive prev/next links are still rendered, and hidden with css,
    to avoid having to supply 3 different variants of display behaviour.

--- generate-film-list/generate.py
+++ generate-film-list/generate.py
@@ -21,7 +21,6 @@ write(html, 'films.html')
 # Generate pages with 20 randomly selected films on each
 file_name_prefix = "films"
 all_titles_list = list(all_films.keys())
-print(len(all_titles_list))
 selection_positions = list(range(len(all_titles_list)))
 random.shuffle(selection_positions)
 
@@ -36,7 +35,20 @@ for position in selection_positions:
 
     if num_films_on_page == 20:
         # Generate page
-        html = template().render(films=films_on_page, next_page=f'{file_name_prefix}-{page_number+1}.html')
+
+        # Workaround for the fact that I can't get Jinja to recognise None
+        html = ''
+        if page_number == 1:
+            html = template().render(
+                films=films_on_page,
+                next_page=f'{file_name_prefix}-{page_number+1}.html'
+            )
+        else:
+            html = template().render(
+                films=films_on_page,
+                prev_page=f'{file_name_prefix}-{page_number-1}.html',
+                next_page=f'{file_name_prefix}-{page_number+1}.html'
+            )
         write(html, f'{file_name_prefix}-{page_number}.html')
         # Move to next page
         page_number += 1
