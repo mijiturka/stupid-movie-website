224c1f30685b98e711155550c0e027bb38828a27
Rumyana <mijiturka@tuta.io>
2021-12-28 00:18:22 +0000

    Add script that generates the films of 2021
    We'll use it for a discussion!

---
+++ generate-film-list/best-of-2021.py
@@ -0,0 +1,11 @@
+import json
+from pathlib import Path
+
+import generate
+
+all = json.loads(Path('./list.json').read_text())['films']
+seen_in_2021 = Path('./seen-in-2021.md').read_text().splitlines()
+
+films = {film: title for (film, title) in all.items() if film in seen_in_2021}
+
+generate.pages_of_lists(films, films_per_page=100)
