acc1d838f95bef8b21731d210db3874efa4426fe
Rumyana <mijiturka@tuta.io>
2021-12-28 00:32:12 +0000

    Add script to regenerate the list of movies right before the new-year smackdown
    We'll probably see a few more until then

---
+++ scripts/get-movies-from-2021.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_DIR="."
+REVIEWS_DIR=${SCRIPT_DIR}"/../reviews"
+
+FILE_TO_WRITE_TO="${SCRIPT_DIR}/../generate-film-list/seen-in-2021.md"
+
+pushd ${REVIEWS_DIR}
+LIST=$(grep -rl "2021" | sed "s/.html//")
+popd
+
+echo "$LIST" > ${FILE_TO_WRITE_TO}
+echo "Results written to ${FILE_TO_WRITE_TO}"
