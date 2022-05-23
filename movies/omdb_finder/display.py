fae2c56bf5aa21032c6875f0944dd76c6749b4eb
Rumyana <mijiturka@tuta.io>
2022-05-23 23:14:50 +0100

    Actually search the OMDB API

--- movies/omdb_finder/display.py
+++ movies/omdb_finder/display.py
@@ -2,10 +2,19 @@ from flask import Flask, abort, render_template, request
 import json
 from pathlib import Path
 
+import client
+
 app = Flask(__name__)
 
 def dummy_response():
     return json.loads(Path('testdata/search_response2.json').read_text())
+
+def actual_search(term):
+    return client.search(term).json()
+
+def clean(input):
+    allowed_symbols = [' ', '-']
+    return ''.join([c for c in input if c.isalpha() or c in allowed_symbols])
 
 @app.route('/')
 def main():
@@ -16,4 +25,7 @@ def search():
     if request.method != 'POST':
         main()
 
-    return render_template('results.html', movies=dummy_response()['Search'])
+    cleaned = clean(request.form.get('search'))
+
+    # return render_template('results.html', movies=dummy_response()['Search'])
+    return render_template('results.html', movies=actual_search(cleaned)['Search'])
