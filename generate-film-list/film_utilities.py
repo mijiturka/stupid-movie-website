fbee3bd3b84ad0ae6a07656641d16fd2e16128f5
Rumyana <mijiturka@tuta.io>
2022-06-06 19:38:26 +0100

    Add function to iterate over all titles from json list

--- generate-film-list/film_utilities.py
+++ generate-film-list/film_utilities.py
@@ -1,5 +1,11 @@
 import json
 from pathlib import Path
 
+def tuples():
+    return json.loads(Path('./list.json').read_text())['films']
+
 def fulltext_title(film):
-    return json.loads(Path('./list.json').read_text())['films'][film]
+    return tuples()[film]
+
+def all_titles():
+    return iter(tuples().values())
