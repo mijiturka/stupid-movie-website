c78566504b5345f637d9ad4f790055794bdb21bf
Rumyana <mijiturka@tuta.io>
2023-01-29 14:52:35 +0000

    Add interactive movie-picking procedure when several are found on our database

--- movies/film.py
+++ movies/film.py
@@ -20,7 +20,51 @@ class Collection:
 
 our_collection = Collection()
 
+class PickyUserException(Exception):
+    pass
+
 class Film:
+    @staticmethod
+    def _presentable_details(c):
+        return (
+            f"{c['Title']} ({c['Year']}) - {c['imdbRating']}/10" + "\n"
+            f"Directed by {c['Director']}" + "\n" +
+            f"Written by {c['Writer']}" + "\n" +
+            f"{c['Plot']}"
+        )
+
+    @staticmethod
+    def _iterate(cursor):
+        # Try to go through peaking at cursor and let the user pick one
+        # We don't iterate through so that, if the user doesn't select anything,
+        # we can offer a final pick without repeating the query.
+        max_attempts = 200
+        for attempt in range(max_attempts):
+            try:
+                c = cursor[attempt]
+            except IndexError as e:
+                # We've reached the end of our results
+                break
+            print(f"{attempt+1} {Film._presentable_details(c)}")
+
+            user_said = input("Is it this one? [y/N]")
+            if user_said in ['y', 'Y', 'yes', 'Yes']:
+                return c
+
+        # Now allow the user to pick one of the movies we've displayed
+        user_said = input(f"Is it any of these? Type a number 1-{attempt} ")
+        if user_said in [str(p) for p in range(1, attempt)]:
+            return cursor[int(user_said)-1]
+
+        # Exhausted options from db
+        user_said = input("Look for it on omdb? [Y/n]")
+        if user_said in ['y', 'Y', 'yes', 'Yes']:
+            logger.info(f"Didn't find {title} ({year}) in our collection. Requesting from omdb")
+            # TODO request
+            pass
+        else:
+            raise PickyUserException("Couldn't find movie after user interrogation")
+
     def __init__(self, title, year):
         # Search our collection first
         cursor = our_collection.get(title=title, year=year)
@@ -36,6 +80,16 @@ class Film:
             next_film = cursor[1]
             logger.info("Found multiple movies. Which one?")
             # TODO iterate and let user choose
+            chosen = Film._iterate(cursor)
+            self.title = chosen['Title']
+            self.year = chosen['Year']
         except IndexError as e:
             logger.debug("We only found 1 movie. Everything is fine")
-            pass
+            self.title = cursor[0]['Title']
+            self.year = cursor[0]['Year']
+        except PickyUserException as e:
+            logger.error(f"Failed to initialise movie: {e}")
+            raise e
+
+    def __str__(self):
+        return f"{self.title} ({self.year})"
